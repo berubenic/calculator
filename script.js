646b8e50e64ee7ecf4b50a6eecd1cbffa80c6651
Nicholas <69174708+berubenic@users.noreply.github.com>
2020-09-04 17:07:17 -0400

    finished basic styling, added add, substract, multiply and divide function

---
+++ script.js
@@ -0,0 +1,20 @@
+//add
+function add(firstNum, secondNum) {
+  result = firstNum + secondNum;
+  return result;
+}
+//substract
+function substract(firstNum, secondNum) {
+  result = firstNum - secondNum;
+  return result;
+}
+//multiply
+function multiply(firstNum, secondNum) {
+  result = firstNum * secondNum;
+  return result;
+}
+//divide
+function divide(firstNum, secondNum) {
+  result = firstnum / secondNum;
+  return result;
+}
